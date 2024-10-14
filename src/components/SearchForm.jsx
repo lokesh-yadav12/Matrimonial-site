49d08f3a08d5695e3ff741f9e88f55c637ca1fc1
lokesh-yadav12 <lokesh916635@gmail.com>
2024-10-15 01:04:16 +0530

    changes-in-app-and-searchform

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -7,7 +7,7 @@ const SearchForm = ({ setShowLogin }) => {
 
     return (
 
-        <div className="text-center bg-neutral-900/40 pt-4 rounded-lg w-full lg:mb-  md:w-[100%] items-center justify-center mx-auto overflow-y-hiddenhidden">
+        <div className="text-center bg-neutral-900/40 p-4 rounded-lg w-full lg:mb-  md:w-[100%] items-center justify-center mx-auto overflow-y-hiddenhidden">
             <div className="   lg:flex flex-wrap gap-2  md:pb-5 mx-auto items-center justify-center">
 
                 <div className="flex gap-2 items-center justify-center">
