9d0f1ae5add9f213e43ceb210fb65a0b0358c647
lokesh-yadav12 <lokesh916635@gmail.com>
2024-10-15 16:01:00 +0530

    change-in-app-add-new-components

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import { Mid1 } from './components/Mid1'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { MatchMakingGuide } from './components/MatchMakingGuide'
+import { Success } from './components/Success'
 
 const App = () => {
   return (
     <div
-      className="absolute h-full w-full inset-0 bg-cover bg-center bg-no-repeat p- m-0"
+      className="absolute h-full w-full inset-0 bg-cover bg-center bg-no-repeat p- m-0 bg-blue-100"
       style={{
         backgroundImage: `url(${bgImage})`,
       }}
@@ -24,19 +25,20 @@ const App = () => {
           <div className=' text-white text-4xl md:text-7xl font-customFont font-bold'>
             Most Verified Partner <br /> Only At Suknvivah
           </div>
-          <div className='w-full flex justify-center items-center pb-10 '>
+          <div className='w-full flex justify-center items-center py-10 '>
             <SearchForm />
           </div>
 
 
         </div>
-        <div className='items-center justify-center mx-auto p-5 bg-blue-100 gap-2'>
+        <div className='items-center justify-center mx-auto p-3 bg-blue-100 '>
           <Mid1 />
         </div>
 
         <div className='py-2 '>
           <MatchMakingGuide/>
         </div>
+        <Success/>
 
       </div>
 
---
+++ src/components/Success.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+// import pic1 from "../images/first.jpeg"
+import pic2 from "../images/second.jpeg"
+import pic3 from "../images/third.jpeg"
+import pic4 from "../images/fourth.jpeg"
+import pic5 from "../images/fifth.jpeg"
+import pic6 from "../images/sixth.jpeg"
+export const Success = () => {
+    return (
+        <div className='justify-center items-center p-2 '  >
+            <h2 className='text-pink-800 text-xl flex p-2 justify-center text-center items-center'><b>Sukhvivah Success Story</b></h2>
+            <div className='lg:flex gap-2 '>
+                <div className='py-2 my-4 items-center justify-center mx-auto border-pink border-2 rounded-3xl'>
+                    <h2 className='h-16  text-center justify-center pt-3 w-[300px] overflow-hidden mx-auto text-white border-pink border-2 rounded-3xl flex bg-pink-900 text-xl'>Sukhvivah Coupling 21/12/2018</h2>
+                    <div className='items-center justify-center mx-auto flex flex-col'>
+
+                        <div className='flex text-pink-800 p-2 w- mx-auto '>
+                            <div>
+                                <img className='h-[135px] w-[100%]' src={pic3} />
+                                <label><b>Lokesh Kumar</b></label>
+                            </div>
+                            <div>
+                                <img className='h-[135px] w-[100%] ' src={pic2} />
+                                <label><b>Nishu yadav</b></label>
+                            </div>
+                        </div>
+                        <div className=''>
+                            <button className='text-white mx-auto bg-pink-800 p-2 text-center '>View success story</button>
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <div className='py-2 my-4 items-center justify-center mx-auto border-pink border-2 rounded-3xl '>
+                    <h2 className='h-16  text-center justify-center pt-3 w-[300px] overflow-hidden mx-auto text-white border-pink border-2 rounded-3xl flex bg-pink-900 text-xl'>Sukhvivah Coupling 21/12/2018</h2>
+                    <div className='items-center justify-center mx-auto flex flex-col'>
+
+                        <div className='flex text-pink-800 p-2 w- mx-auto '>
+                            <div>
+                                <img className='h-[135px] w-[100%]' src={pic3} />
+                                <label><b>Siddhant Nema</b></label>
+                            </div>
+                            <div>
+                                <img className='h-[135px] w-[100%] ' src={pic4} />
+                                <label><b>Riya Bajaj</b></label>
+                            </div>
+                        </div>
+                        <div className=''>
+                            <button className='text-white mx-auto bg-pink-800 p-2 text-center '>View success story</button>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className='py-2 my-2 items-center justify-center mx-auto  border-pink border-2 rounded-3xl'>
+                    <h2 className='h-16  text-center justify-center pt-3 w-[300px] overflow-hidden mx-auto border-pink border-2 rounded-3xl text-white flex bg-pink-900 text-xl'>Sukhvivah Coupling 21/12/2018</h2>
+                    <div className='items-center justify-center mx-auto flex flex-col'>
+
+                        <div className='flex text-pink-800 p-2 w- mx-auto '>
+                            <div>
+                                <img className='h-[135px] w-[100%]' src={pic5} />
+                                <label><b>Ayush Raj</b></label>
+                            </div>
+                            <div>
+                                <img className='h-[135px] w-[100%] ' src={pic4} />
+                                <label><b>Aditi</b></label>
+                            </div>
+                        </div>
+                        <div className=''>
+                            <button className='text-white mx-auto bg-pink-800 p-2 text-center '>View success story</button>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+}
